90008541eb077ccd762a4711e3d965c12a032950
Land-Maze <timurjes@gmail.com>
2021-02-12 03:24:58 +0200

    UDP function is working v2

--- Addons/logical.hpp
+++ Addons/logical.hpp
@@ -271,13 +271,4 @@ class Logical {
         }
 
     }
-
-    wchar_t *GetWC(const char *c){
-
-    const size_t cSize = strlen(c)+1;
-    wchar_t* wc = new wchar_t[cSize];
-    mbstowcs (wc, c, cSize);
-
-    return wc;
-    }
 };
